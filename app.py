be3e0a0af20f3f22ce56fd5393e2cf5c940de537
T.J. Kolberg <tkolberg@asu.edu>
2021-07-25 23:43:58 -0600

    🍥  The Main Application

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from system_functions import clear_screen
 from twilio_functions import twilio_call, twilio_text
-from secrets import coins
+from secrets import coins, my_number
 from crypto_data import get_crypto_price
 from time import sleep
 
@@ -30,7 +31,37 @@ def determine_extra_coins():
     return extra
 
 
-if __name__ == '__main__':
-    
-    
+# Determine the amount of Coin 2 the user wishes to receive from transferring Coin 1
+def get_coin_goal():
 
+    try:
+        
+        goal_amount = float(input('How much of Coin 2 are you looking to receive?\n'))
+
+    except ValueError:
+
+        print("Numbers only, please try again...")
+        get_coin_goal()
+    
+    else:
+
+        return goal_amount
+
+
+
+if __name__ == '__main__':
+
+    clear_screen()
+
+    goal_amount = get_coin_goal()
+    
+    while True:
+
+        extra = determine_extra_coins()
+        print(extra)
+
+        if extra >= goal_amount:
+
+            twilio_text(f'Trade now!\nConversion to {extra} coins!', my_number)
+            twilio_call(my_number)
+        
