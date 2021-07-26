08a0e6f9514af750683e8655deaecd8c56fca37d
T.J. Kolberg <tkolberg@asu.edu>
2021-07-25 23:28:24 -0600

    🍥  The Main Application

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from twilio_functions import twilio_call, twilio_text
+from secrets import coins
+from crypto_data import get_crypto_price
 
 
 if __name__ == '__main__':
     
-    print('code this')
+    # Declare empty list for coin values
+    values = list()
+
+    # Retrieve coin prices
+    for coin in coins:
+
+        if coin == dict:
+            print('yes')
+
+        # coin_value = get_crypto_price(coin)
+        # values.append(coin_value)
+    
+    print(values)
+
+
