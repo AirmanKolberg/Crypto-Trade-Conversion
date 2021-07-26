ef4a0da72417be77a51a820c2d7180c0cc3dca8d
T.J. Kolberg <tkolberg@asu.edu>
2021-07-25 23:36:40 -0600

    🍥  The Main Application

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 from twilio_functions import twilio_call, twilio_text
 from secrets import coins
 from crypto_data import get_crypto_price
+from time import sleep
 
 
-if __name__ == '__main__':
-    
+def determine_extra_coins():
+
     # Declare empty list for coin values
     values = list()
+
+    # Declare ticker to multiply only sell-coin by amount held
+    ticker = 0
 
     # Retrieve coin prices
     for coin in coins:
 
-        if coin == dict:
-            print('yes')
+        coin_value = get_crypto_price(coin)
 
-        # coin_value = get_crypto_price(coin)
-        # values.append(coin_value)
+        if ticker == 0:
+            
+            # Multiply value by amount
+            coin_value = (coin_value * coins[coin]).__round__(2)
+            ticker += 1
+
+        values.append(coin_value)
     
-    print(values)
+    extra = (values[0] / values[1]).__round__(2)
 
+    return extra
+
+
+if __name__ == '__main__':
+    
+    
 
