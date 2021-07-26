90eb43b6cc63b72cb881b44fdf3554eb9414d511
T.J. Kolberg <tkolberg@asu.edu>
2021-07-25 23:28:27 -0600

    🤐  Example of a secrets.py File

--- secrets_example.py
+++ secrets_example.py
@@ -5,7 +5,11 @@ twilio_number = 'goes'
 my_number = 'here'
 
 # Crypto information
-coins = ['ETH', 'ADA']
+coins = {
+    'ETH': 4.7,     # Coin to sell
+    'ADA': 10000    # Coin to buy
+  }
+
 
 
 if __name__ == '__main__':
