a91b65a07049e119c4a320864de6b1028acfa679
T.J. Kolberg <tkolberg@asu.edu>
2021-07-25 23:05:05 -0600

    💬  Twilio API Functionality

--- twilio_functions.py
+++ twilio_functions.py
@@ -5,22 +5,18 @@ from secrets import sid, token, twilio_number
 client = Client(sid, token)
 
 
-def twilio_call(numbers_to_call):
-
-    for number in numbers_to_call:
+def twilio_call(number_to_call):
     
-        client.calls.create(url='http://demo.twilio.com/docs/voice.xml',
-                            to=number,
-                            from_=twilio_number)
+    client.calls.create(url='http://demo.twilio.com/docs/voice.xml',
+                        to=number_to_call,
+                        from_=twilio_number)
 
 
-def twilio_text(text_message, numbers_to_text):
+def twilio_text(text_message, number_to_text):
 
-    for number in numbers_to_text:
-
-        client.messages.create(body=text_message,
-                               from_=twilio_number,
-                               to=number)
+    client.messages.create(body=text_message,
+                            from_=twilio_number,
+                            to=number_to_text)
 
 
 if __name__ == '__main__':
